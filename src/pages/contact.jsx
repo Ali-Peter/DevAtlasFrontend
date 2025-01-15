f82bfe7f427436df357b98263aca96d3f7c11dbe
Ali-Peter <aliogochukwu06@gmail.com>
2025-01-15 06:33:14 +0100

    modified:   src/pages/contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://portfolio-server-side-kmi7.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
